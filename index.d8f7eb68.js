6ed188c2c32ac30a499abfa686fc27b606785c4a
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-11-22 10:05:46 +0000

    Deploying to gh-pages from @ VuktorTS/js-team-project-FoodStore@f8ddcf67cfb671f8f90152d423a5f5bf8d725346 🚀

---
+++ index.d8f7eb68.js
@@ -0,0 +1,2 @@
+console.log("header"),console.log("filters"),console.log("pagination"),console.log("popular"),console.log("discount"),console.log("footer"),console.log("cart");
+//# sourceMappingURL=index.d8f7eb68.js.map
