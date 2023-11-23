27e3b026c8b892f4453eb4b7af2baeb0a687d420
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-11-23 19:55:51 +0200

    api

--- src/js/helpers/food-api.js
+++ src/js/helpers/food-api.js
@@ -2,28 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://food-boutique.b.goit.study/api';
 
- export class ProductsAPI {
-   constructor () {
-    this.page = 1;
-    this.limit = 5;
-    this.keyword = '';
-    this.category = '';
-    this.byABC = false;
-    this.byPrice = false;
-    this.byPopularity = false;
-    this.id ='';
-  }
+export class ProductsAPI {
+  constructor() {}
 
-  async getProducts() {
+  async getProducts(newParametrs) {
     const response = await axios.get('/products', {
       params: {
-        page: this.page,
-        limit: this.limit,
-        keyword: this.keyword,
-        category: this.category,
-        byABC: this.byABC,
-        byPrice: this.byPrice,
-        byPopularity: this.byPopularity,
+        ...newParametrs,
       },
     });
     return response.data;
@@ -32,44 +17,24 @@ axios.defaults.baseURL = 'https://food-boutique.b.goit.study/api';
   async getPopularProducts() {
     const response = await axios.get('/products/popular', {
       params: {
-        page: this.page,
-        limit: this.limit,
-        byPopularity: this.byPopularity,
+        ...newParametrs,
       },
     });
     return response.data;
   }
 
   async getDiscountProducts() {
-    const response = await axios.get('/products/discount', {
-      params: {
-        page: this.page,
-        limit: this.limit,
-        byPrice: this.byPrice,
-      },
-    });
+    const response = await axios.get('/products/discount');
     return response.data;
   }
 
   async getProductCategories() {
-    const response = await axios.get('/products/categories', {
-      params: {
-        page: this.page,
-        limit: this.limit,
-        category: this.category,
-      },
-    });
+    const response = await axios.get('/products/categories');
     return response.data;
   }
 
   async getProductById(id) {
-    const response = await axios.get(`/products/${id}`, {
-      params: {
-        page: this.page,
-        limit: this.limit,
-        id: this.id,
-      },
-    });
+    const response = await axios.get(`/products/${id}`);
     return response.data;
   }
 
@@ -91,4 +56,3 @@ axios.defaults.baseURL = 'https://food-boutique.b.goit.study/api';
     return response.data;
   }
 }
-
