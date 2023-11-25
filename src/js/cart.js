42fce72daacfd325a448f546260949d992c24a44
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-11-25 16:05:08 +0200

    change style

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,9 +2,3 @@ import Scrollbar from 'smooth-scrollbar';
 Scrollbar.init(document.querySelector('#my-scrollbar'), {
   alwaysShowTracks: true,
 });
-import { ProductsAPI } from './helpers/food-api';
-const productsApi = new ProductsAPI();
-productsApi.getProducts({ category: 'Prepared_Foods' }).then(res => {
-  console.log('res: ', res);
-});
-console.log('ok');
