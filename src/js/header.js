534bb15481a73cc005a6867096c787017406fd34
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-11-25 16:06:27 +0200

    header js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,8 @@
-console.log('header')
+const cartCounter = document.getElementById('cart-counter');
+const cartCount = parseInt(cartCounter.textContent);
+function redirectToCartPage() {
+window.location.href = 'cart.html';
+}
+document.querySelector('.shopping-cart-link').addEventListener('click', redirectToCartPage);
+const updatedCartCount = 0;
+cartCounter.textContent = `(${updatedCartCount})`;
