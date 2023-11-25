fcddb06f8a6c51eeb326c5c0482f86ec0a967971
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-11-25 14:22:55 +0200

    add smooth-scrollbar

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,1 +1,10 @@
-console.log('cart');
+import Scrollbar from 'smooth-scrollbar';
+Scrollbar.init(document.querySelector('#my-scrollbar'), {
+  alwaysShowTracks: true,
+});
+import { ProductsAPI } from './helpers/food-api';
+const productsApi = new ProductsAPI();
+productsApi.getProducts({ category: 'Prepared_Foods' }).then(res => {
+  console.log('res: ', res);
+});
+console.log('ok');
