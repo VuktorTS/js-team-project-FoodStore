434810be2e25e3ea37525d9e47ef067c94bf60ba
AndriiPolishchuk28 <andrey.work28@gmail.com>
2023-11-25 19:09:59 +0100

    filters

--- src/js/filters.js
+++ src/js/filters.js
@@ -5,15 +5,51 @@ import localStorage from './helpers/local-storage';
 import 'npm:slim-select/dist/slimselect.css';
 import '../css/index.css';
 
+function createCardsMarkup(products) {
+  const markupCardArray = products.map(
+    ({ img, name, category, size, popularity, price } = products) => {
+      return `
+      <li class="card">
+      <div class="bg_img">
+        <img src="${img}" class="img_card" alt="" />
+      </div>
+      <div class="text_wrapped">
+        <h3 class="text_name_prod">${name}</h3>
+        <div class="wrapper_info">
+          <p class="text_title">Category:</p>
+          <span class="text_subtitle">${category}</span>
+          <p class="text_title">Size:</p>
+          <span class="text_subtitle">${size}</span>
+          <p class="text_title">Popularity:</p>
+          <span class="text_subtitle">${popularity}</span>
+        </div>
+      </div>
+      <div class="wrapper_price">
+        <span class="text_price">${price}</span>
+        <span>
+          <svg class="cart_svg" width="18" height="18">
+            <use href="./images/icons.svg#icon-shopping-cart"></use>
+          </svg>
+        </span>
+      </div>
+    </li>
+      `;
+    }
+  );
+
+  return markupCardArray.join('');
+}
+
 const storageSave = localStorage.loadFromLocalStorage(FILTER_KEY);
 
 const filters = {
   categories: new ProductsAPI(),
-  inputRef: document.querySelector('filter'),
   formRef: document.querySelector('.form_filters'),
+  ulRef: document.querySelector('.wrapper_card'),
   select: new SlimSelect({
     events: {
       beforeChange: newVal => {
+        getProductsCategory(newVal[0].value);
         localStorage.saveToLocalStorage(FILTER_KEY, {
           ...storageSave,
           categories: newVal[0].value,
@@ -23,14 +59,82 @@ const filters = {
     select: '#categories',
     settings: {
       showSearch: false,
-      placeholderText: getValueCategories(),
+      placeholderText: !getValueCategories()
+        ? 'Categories'
+        : getValueCategories(),
     },
   }),
 };
+checkStorage();
 
 filters.formRef.addEventListener('submit', onSubmit);
 
-filters.categories.getProducts('Deli').then(data => console.log(data));
+function getValueCategories() {
+  if (!localStorage.loadFromLocalStorage(FILTER_KEY)) {
+    return null;
+  }
+  return localStorage.loadFromLocalStorage(FILTER_KEY).categories;
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { search } = filters.formRef;
+  console.log(storageSave);
+  localStorage.saveToLocalStorage(FILTER_KEY, {
+    ...storageSave,
+    keyword: search.value,
+  });
+  const products = localStorage.loadFromLocalStorage('products');
+  const value = search.value.toLowerCase();
+  if (search.value.length) {
+    const newArr = products.filter(el => el.name.toLowerCase().includes(value));
+    filters.ulRef.innerHTML = createCardsMarkup(newArr);
+  } else {
+    const category = localStorage.loadFromLocalStorage(FILTER_KEY).categories;
+    getProductsCategory(category);
+  }
+}
+
+async function renderMarkUpProducts() {
+  const result = await fetchProducts();
+  const markup = createCardsMarkup(result);
+  filters.ulRef.insertAdjacentHTML('beforeend', markup);
+}
+
+async function getProductsCategory(value) {
+  const response = await filters.categories.getProducts({
+    category: value,
+  });
+  const result = await response.results;
+  localStorage.saveToLocalStorage('products', result);
+
+  const keyword = localStorage.loadFromLocalStorage(FILTER_KEY).keyword;
+  if (keyword) {
+    const products = localStorage.loadFromLocalStorage('products');
+    const filteredArr = products.filter(el => {
+      el.name.toLowerCase().includes(keyword);
+    });
+    filters.ulRef.innerHTML = createCardsMarkup(filteredArr);
+    return;
+  }
+  const markup = createCardsMarkup(result);
+  filters.ulRef.innerHTML = markup;
+}
+
+function checkStorage() {
+  if (storageSave) {
+    getProductsCategory(storageSave.categories);
+    return;
+  }
+  renderMarkUpProducts();
+}
+
+async function fetchProducts() {
+  const response = await filters.categories.getProducts();
+  const result = await response.results;
+  return result;
+}
+
 filters.categories
   .getProductCategories()
   .then(data => {
@@ -45,19 +149,3 @@ filters.categories
     const text = getValueCategories();
     filters.select.setData([{ placeholder: true, text: text }, ...data]);
   });
-
-function getValueCategories() {
-  if (!localStorage.loadFromLocalStorage(FILTER_KEY)) {
-    return 'Categories';
-  }
-  return localStorage.loadFromLocalStorage(FILTER_KEY).categories;
-}
-
-function onSubmit(e) {
-  e.preventDefault();
-  const { search } = filters.formRef;
-  localStorage.saveToLocalStorage(FILTER_KEY, {
-    ...storageSave,
-    search: search.value,
-  });
-}
