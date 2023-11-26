4de7a97c183f21b1409f8f93d0dcd702aab0886c
AndriiPolishchuk28 <andrey.work28@gmail.com>
2023-11-27 00:48:57 +0100

    modal_popular

--- src/js/popular.js
+++ src/js/popular.js
@@ -3,64 +3,15 @@ import { ProductsAPI } from './helpers/food-api';
 import {
   saveToLocalStorage,
   loadFromLocalStorage,
-  removeFromLocalStorage,
 } from './helpers/local-storage';
 
-function createPopularProductsMarkup(products) {
-  const markupPopularProductsArray = products.map(
-    ({ img, name, category, size, popularity, _id }) => {
-      return `
-        <li class="popular-item" id = "${_id}"><div class="product-card">
-        <div class="popular-product-icon"><img src="${img}" alt="${name}"></div>
-        <!-- Картки зображень -->
-        <div class="product-details">
-            <p class="popular-name">${name}</p>
-        <p class="popular-text">Category: <span class="popular-span popular-category">${category}</span></p>
-        <div class="row">
-        <p class="popular-text">Size: <span class="popular-span popular-size">${size}</span></p>
-        <p class="popular-text">Popularity: <span class="popular-span popular-size">${popularity}</span></p>
-    </div>
-        </div>
-        <button class="add-to-bascket" data-id="${_id}"></button>
-      </div></li>
-        `;
-    }
-  );
+import {
+  createPopularProductsMarkup,
+  createDiscountProductsMarkup,
+} from './helpers/create-markup';
 
-  return markupPopularProductsArray.join('');
-}
-
-function createDiscountProductsMarkup(products) {
-  const markupDiscountProductsArray = products.map(
-    ({ img, name, _id, price }) => {
-      return `
-      <li class="discount-item" id = "${_id}">
-      <div class="discount-image-bg">
-        <img src="${img}" alt="${name}" class="discount-image" />
-      </div>
-      <div class="discount-info">
-        <h3 class="discount-name">${name}</h3>
-        <div class="discount-price-btn-wrapper">
-          <p class="discount-price">${price}</p>
-          <button type="button" class="button discount-btn" data-id="${_id}">
-          </button>
-        </div>
-      </div>
-      <img
-        src="./images/discount.png"
-        alt="discount sticker"
-        class="discount-sticker"
-      />
-    </li>
-      `;
-    }
-  );
-
-  return markupDiscountProductsArray.join('');
-}
-
-function modalPopular(product) {
-  return `<div class="backdrop is-hidden" data-modal>
+function modalPopular({ name, category, size, popularity, img, price, desc }) {
+  return `
   <div class="modal">
     <button type="button" class="modal-close-btn" data-modal-close>
       <svg class="modal-close-icon">
@@ -70,31 +21,30 @@ function modalPopular(product) {
     <div class="modal-content">
       <div class="modal-main-row">
         <div class="modal-image-bg">
-          <img src="./images/image2.png" alt="" class="modal-image" />
+          <img src="${img}" alt="" class="modal-image" />
         </div>
         <div class="modal-description-wrapper">
           <div class="modal-list-wrapper">
-            <h3 class="modal-product-name">Pumpkin</h3>
+            <h3 class="modal-product-name">${name}</h3>
             <ul class="modal-list">
               <li class="modal-list-item">
-                <p>Category: <span class="modal-span">Fresh Produce</span></p>
+                <p>Category: <span class="modal-span">${category}</span></p>
               </li>
               <li class="modal-list-item">
-                <p>Size: <span class="modal-span">1 medium-sized</span></p>
+                <p>Size: <span class="modal-span">${size}</span></p>
               </li>
               <li class="modal-list-item">
-                <p>Popularity: <span class="modal-span">0</span></p>
+                <p>Popularity: <span class="modal-span">${popularity}</span></p>
               </li>
             </ul>
           </div>
           <p class="modal-text">
-            A round, orange vegetable with a thick shell and seeds inside. It is
-            often used in cooking and baking for its sweet and earthy flavor.
+            ${desc}
           </p>
         </div>
       </div>
       <div class="modal-bottom-row">
-        <h3 class="modal-price">$2.99</h3>
+        <h3 class="modal-price">${price}$</h3>
         <button type="button" class="modal-btn">
           Add to
           <svg class="modal-btn-icon">
@@ -104,17 +54,23 @@ function modalPopular(product) {
       </div>
     </div>
   </div>
-</div>`;
+`;
 }
 
 const popular = {
   api: new ProductsAPI(),
   popularRef: document.querySelector('.popular-container'),
   discountRef: document.querySelector('.discount-list'),
+  wrapperModal: document.createElement('div'),
 };
 popular.popularRef.addEventListener('click', onClickPopular);
 renderPopular();
 renderDiscounts();
+
+popular.wrapperModal.classList.add('backdrop');
+popular.wrapperModal.classList.add('is-hidden');
+popular.wrapperModal.setAttribute('data', 'modal');
+document.body.appendChild(popular.wrapperModal);
 
 async function renderPopular() {
   const response = await popular.api.getPopularProducts();
@@ -130,11 +86,43 @@ async function renderDiscounts() {
   const markup = createDiscountProductsMarkup(result);
   popular.discountRef.innerHTML = markup;
 }
+const modal = document.querySelector('.backdrop');
 
 function onClickPopular(e) {
   const popUpElement = e.target.closest('li');
   if (popUpElement) {
-    const arr = storage.loadFromLocalStorage(POPULAR_KEY);
-    const selected = arr.find(el => el._id === popUpElement.id);
+    fetchById(popUpElement.id)
+      .then(data => {
+        const markUp = modalPopular(data);
+        displayModal(markUp);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }
+}
+
+function displayModal(markUp) {
+  if (markUp) {
+    modal.innerHTML = markUp;
+    modal.classList.remove('is-hidden');
+    setupCloseButton();
+  }
+}
+
+function setupCloseButton() {
+  const closeBtn = document.querySelector('.modal-close-btn');
+  closeBtn.addEventListener('click', () => {
+    closeModal();
+  });
+}
+
+function closeModal() {
+  modal.classList.add('is-hidden');
+  modal.innerHTML = '';
+}
+
+async function fetchById(id) {
+  const response = await popular.api.getProductById(id);
+  return response;
 }
