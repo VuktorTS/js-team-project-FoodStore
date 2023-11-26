dc295c33c8538898d93e1bf2afafc7aba10439f8
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-11-26 18:51:33 +0200

    header js

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,4 @@
 import {CART_KEY} from "./helpers/storage-keys";
 const cartCounter = document.getElementById('cart-counter');
 const updatedCartCount = 0;
-cartCounter.textContent = `(${CART_KEY})`;
+cartCounter.textContent = `(${'cart'})`;
