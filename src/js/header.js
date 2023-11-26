cfa24722934f91f5a16b9401303dc4727ce01671
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-11-27 00:01:36 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,12 @@
-import {saveToLocalStorage, loadFromLocalStorage} from "./helpers/local-storage";
-import {CART_KEY} from "./helpers/storage-keys";
+import {
+  saveToLocalStorage,
+  loadFromLocalStorage,
+} from './helpers/local-storage';
+import { CART_KEY } from './helpers/storage-keys';
 
 const count = loadFromLocalStorage(CART_KEY);
 // console.log('count:',count);
 const cartCounter = document.getElementById('cart-counter');
-cartCounter.textContent = `(${count.length})`;
+if (count) {
+  cartCounter.textContent = `(${count.length})`;
+}
