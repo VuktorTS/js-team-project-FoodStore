36e232e6bd2341ac2e4ed27e00781bfd3bca7325
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-11-26 20:18:38 +0200

    header

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,7 @@
+import {saveToLocalStorage, loadFromLocalStorage} from "./helpers/local-storage";
 import {CART_KEY} from "./helpers/storage-keys";
+
+const count = loadFromLocalStorage(CART_KEY);
+// console.log('count:',count);
 const cartCounter = document.getElementById('cart-counter');
-const updatedCartCount = 0;
-cartCounter.textContent = `(${'cart'})`;
+cartCounter.textContent = `(${count.length})`;
