75c2dfc853dcb8e5df2912ca581a35cb289ffb23
VovaDev777 <polvovater@gmail.com>
2023-11-29 18:03:39 +0100

    pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,34 @@
-console.log('pagination')
+import Pagination from 'tui-pagination';
+import '../css/index.css';
+import '../css/pagination.css';
+
+
+// Update your options object
+const options = {
+    totalItems: 500,
+    itemsPerPage: 9,
+    visiblePages: 4,
+    page: 1,
+    centerAlign: false,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    },
+   
+  };
+  
+  const pagination = new Pagination('pagination', options);
